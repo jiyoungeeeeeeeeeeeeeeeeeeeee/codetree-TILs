e44e5053fde5a63d1dd3266b718370f0b9ef401b
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2024-08-08 17:13:22 +0900

    Title: 특정 조건 세 정수 비교, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240808/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -0,0 +1,24 @@
+a,b,c = list(map(int,input().split()))
+
+
+# c가 최솟값일 경우
+if a > c and b > c :
+    if a == c :
+        print(1 , end = ' ')
+    else :
+        print(0, end = ' ')
+
+# b가 최솟값일 경우
+if a > b and c > b:
+    if a == b:
+        print(1, end = ' ')
+    else:
+        print(0, end = ' ')
+
+#a가 최솟값일 경우
+if c > a and b > a :
+  print(1, end = ' ')
+
+if a==b==c:
+    print(1, end = ' ')
+else: print(0, end = ' ')
