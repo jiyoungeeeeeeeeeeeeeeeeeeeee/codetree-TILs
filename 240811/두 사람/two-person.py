d7f65e693d046e9de55bc60bd7ebb0102b0de884
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2024-08-11 20:46:26 +0900

    Title: 두 사람, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240811/두 사람/two-person.py
+++ 240811/두 사람/two-person.py
@@ -1,10 +1,16 @@
 a , s1  = input().split()
 b , s2  = input().split()
 
-if a or b >= 19:
-    
+a = int(a)
+b = int(b)
+
+
+if a and b >= 19 and s1 and s2 == 'M':
+    print(1)
+
+elif a or b >= 19:    
     if s1 or s2 == 'M':
         print(1)
-    else : print(0)
+    else: print(0)
 
 else: print(0)
