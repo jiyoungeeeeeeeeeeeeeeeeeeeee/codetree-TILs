e06e52c46215887c337167e5cf1813c82073a4da
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2024-08-11 21:24:58 +0900

    Title: 윤년인가, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240811/윤년인가/is-leap-year.py
+++ 240811/윤년인가/is-leap-year.py
@@ -2,7 +2,6 @@ y = int(input())
 
 if y%4 == 0 :
     print('true')
-
-if y% 100 == 0:
-    if y%400 != 0:
-        print("false")
+    if y% 100 == 0:
+        if y%400 != 0:
+            print("false")
