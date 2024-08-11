02f8be2e6535a37fa8c405d34c77773fc05fbab4
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2024-08-11 21:01:07 +0900

    Title: 두 사람, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 240811/두 사람/two-person.py
+++ 240811/두 사람/two-person.py
@@ -8,7 +8,7 @@ b = int(b)
 if (a >= 19 and s1 == "M" ) and ( b >= 19 and s2 == 'M'):
     print(1)
 
-elif (a or b >= 19 ) and (s1 or s2 == 'M'):
+elif (a >= 19 and s1 == "M" ) or ( b >= 19 and s2 == 'M'):
     print(1)
    
 
