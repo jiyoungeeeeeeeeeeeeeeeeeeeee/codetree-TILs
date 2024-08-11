4bbd9a6527525f1cfd18355babfc82f818427e15
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2024-08-11 20:58:23 +0900

    Title: 두 사람, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240811/두 사람/two-person.py
+++ 240811/두 사람/two-person.py
@@ -5,7 +5,7 @@ a = int(a)
 b = int(b)
 
 
-if (a and b >= 19 )and (s1 and s2 == 'M'):
+if (a >= 19 and s1 == "M" ) and ( b >= 19 and s2 == 'M'):
     print(1)
 
 elif (a or b >= 19 ) and (s1 or s2 == 'M'):
