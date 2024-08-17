30aafc5157049334410f2207ea64b2a91079d01b
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2024-08-18 02:32:46 +0900

    Title: 완전수 판별기, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240818/완전수 판별기/perfect-number-discriminator.py
@@ -0,0 +1,11 @@
+sum_val = 0
+
+n = int(input())
+
+for i in range(1,n+1):
+    if n%i == 0:
+        sum_val += i
+        if sum_val == n:
+            print('P')
+    elif sum_val != n:
+         print('N')
