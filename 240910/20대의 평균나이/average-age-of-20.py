35a0c97b071acec9b7681a6433f8fcb962902572
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2024-09-10 23:32:29 +0900

    Title: 20대의 평균나이, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240910/20대의 평균나이/average-age-of-20.py
@@ -0,0 +1,15 @@
+sum = 0
+count = 0
+
+while True:
+
+    n = int(input())
+   
+    if 20 <= n <= 29:
+        sum += n
+        count += 1
+        
+    
+  
+    else : break
+print (f"{sum/count:.2f}")
